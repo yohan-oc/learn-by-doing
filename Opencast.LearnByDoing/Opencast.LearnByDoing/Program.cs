8f96d146452550a6aa3e0e681f5ded96292861eb
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-10-24 14:38:38 +0100

    puzzle 01 solution

--- Opencast.LearnByDoing/Opencast.LearnByDoing/Program.cs
+++ Opencast.LearnByDoing/Opencast.LearnByDoing/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using Opencast.LearnByDoing.Puzzle01;
 
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Learn by Doing!");
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+Puzzle01.Run();
+
+stopwatch.Stop();
+Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
---
+++ Opencast.LearnByDoing/Opencast.LearnByDoing/Puzzle01/Puzzle01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Opencast.LearnByDoing.Puzzle01
+{
+	public static class Puzzle01
+	{
+		public static void Run()
+		{
+            string[] lines = File.ReadAllLines("Puzzle01/puzzle_bonus.csv");
+
+            var clientIdSetCache = new HashSet<string>(StringComparer.Ordinal);
+
+            var duplicates = new List<string>();
+
+			for(int i=0; i < lines.Length; i++)
+			{
+                var splitLine = lines[i].Split(",");
+
+                string serialNo = splitLine[0];
+				string clientId = splitLine[1];
+
+				if (clientIdSetCache.Contains(clientId))
+				{
+					//Console.WriteLine("Duplicate row: " + serialNo);
+					duplicates.Add(lines[i]);
+					break;
+				}
+				else
+				{
+                    clientIdSetCache.Add(clientId);
+				}
+            }
+
+			Console.WriteLine("Size of the dictionary: " + string.Join(", ", duplicates));
+        }
+	}
+}
+
