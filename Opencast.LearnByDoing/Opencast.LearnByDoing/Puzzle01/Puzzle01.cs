d19ebc1096c2d4a16769ca2840d28c8820ca55d3
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-10-24 16:44:32 +0100

    tidy up

--- Opencast.LearnByDoing/Opencast.LearnByDoing/Puzzle01/Puzzle01.cs
+++ Opencast.LearnByDoing/Opencast.LearnByDoing/Puzzle01/Puzzle01.cs
@@ -1,36 +1,35 @@
-﻿using System;
-namespace Opencast.LearnByDoing.Puzzle01
+﻿namespace Opencast.LearnByDoing.Puzzle01
 {
-	public static class Puzzle01
+    public static class Puzzle01
 	{
 		public static void Run()
 		{
-            string[] lines = File.ReadAllLines("Puzzle01/puzzle_bonus.csv");
+			string[] lines = File.ReadAllLines("Puzzle01/puzzle_bonus.csv");
 
-            var clientIdSetCache = new Dictionary<string, string>(StringComparer.Ordinal);
+			var clientIdSetCache = new Dictionary<string, string>(StringComparer.Ordinal);
 
-            List<string> duplicateSerialNumbers = new();
+			List<string> duplicateSerialNumbers = new();
 
-			for(int i=0; i < lines.Length; i++)
+			for (int i = 0; i < lines.Length; i++)
 			{
-                var splitLine = lines[i].Split(",");
-                string serialNo = splitLine[0];
-                string clientId = splitLine[1];
+				var splitLine = lines[i].Split(",");
+				string serialNo = splitLine[0];
+				string clientId = splitLine[1];
 
 				if (clientIdSetCache.ContainsKey(clientId))
 				{
-                    duplicateSerialNumbers.Add(clientIdSetCache[clientId]);
-                    duplicateSerialNumbers.Add(serialNo);
-                    break;
+					duplicateSerialNumbers.Add(clientIdSetCache[clientId]);
+					duplicateSerialNumbers.Add(serialNo);
+					break;
 				}
 				else
 				{
-                    clientIdSetCache.Add(clientId, serialNo);
+					clientIdSetCache.Add(clientId, serialNo);
 				}
-            }
+			}
 
 			Console.WriteLine("Duplicate serial numbers: " + string.Join(", ", duplicateSerialNumbers));
-        }
+		}
 	}
 }
 
